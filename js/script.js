127fe4f51989a14461c6d261110c0b0c63ffd4fc
Chantel T <chantel.theodule@outlook.com>
2022-01-31 18:53:44 -0500

    Blog Site

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+let likeIt = document.getElementById("like-btn")
+likeIt.addEventListener("click", () => {
+    likeIt.innerHTML = "&#128077;"
+})
+
+let heartIt = document.getElementById("heart-btn")
+ heartIt.addEventListener("click", () => {
+ heartIt.innerHTML = "&#128525;"
+ })
+
+ let checkIt = document.getElementById("check-btn")
+ checkIt.addEventListener("click", () => {
+ checkIt.innerHTML = "&#10004;"
+ })
